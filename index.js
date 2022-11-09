e752c3767a6889bb5f14df8586f008705935d8d6
Sumaiya-topu <topusumaiya@gmail.com>
2022-11-09 22:21:46 +0600

    first get api created

--- index.js
+++ index.js
@@ -15,12 +15,21 @@ console.log(process.env.DB_USER, process.env.DB_PASSWORD);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qkzu9ty.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        const serviceCollection = client.db('painted-lady').collection("services");
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    } finally {
+
+    }
+}
+run().catch(error => console.error(error))
 
 app.get('/', (req, res) => {
     res.send('simple server running');
